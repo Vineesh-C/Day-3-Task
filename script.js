82d850638f50c0d10ce537ba167019294874c726
Vineesh-C <112311989+Vineesh-C@users.noreply.github.com>
2022-12-24 17:49:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let myResume={
+    "basics": {
+      "name": "VINEESH C",
+      "email": "vineesh.c16@gamil.com",
+      "phone": 9941990916,
+      "degree": "MBA",
+      "location": {
+        "address": "No 1 Medavakkam tank rd, Ramalingapuram",
+        "postalCode": 600012,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India",
+      },
+      
+    },
+    "work": [
+      {
+        "Total experience" : "5 years",
+        "first company" : "TCS" ,
+        "start date" : "2017-12-11",
+        "end date" : "2020-12-31",
+        "company": "Wipro pvt ltd",
+        "position": "Senior officer",
+        "startDate": "2021-05-21",
+        "endDate": "Till date",
+        "summary": "i am the process executive handling reconsilation and journal entries experince",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Loyola college of pondichery university",
+        "department": "MBA",
+        "studyType": "Parttime",
+        "batch start year": 2017,
+        "batch end year": 2019,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Malayalam,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "cricket, learn new things",
+      }
+    ]
+  }
+  console.log(myResume);
